c04212578b37fc8d7a8e3a1ce0dd1e4d9cb5e4a5
nekokanbaru <mihailorang@gmail.com>
2024-11-26 22:28:11 +0100

    add dificulties

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -5,10 +5,12 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Math Game</Text>
-      <Button 
-        title="Play" 
-        onPress={() => navigation.navigate('Game')} 
-      />
+      <View style={styles.button}>
+        <Button 
+          title="Play"
+          onPress={() => navigation.navigate('Difficulty')} 
+        />
+      </View>
     </View>
   );
 };
@@ -16,15 +18,20 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 100,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f5f5f5',
   },
   title: {
-    fontSize: 32,
+    fontSize: 52,
     fontWeight: 'bold',
     marginBottom: 20,
   },
+  button: {
+    width: 100,
+    fontSize: 30,
+  }
 });
 
 export default HomeScreen;
