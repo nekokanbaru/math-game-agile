abe8f4ab72618961c60c3c578cd119fb63b3abf7
nekokanbaru <mihailorang@gmail.com>
2024-12-17 19:10:56 +0100

    add pause screen

--- screens/SelectLevelScreen.jsx
+++ screens/SelectLevelScreen.jsx
@@ -1,53 +1,90 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, ImageBackground, TouchableOpacity, Text } from 'react-native';
 
 const SelectLevelScreen = ({ route, navigation }) => {
     const { difficulty } = route.params;
     const levels = require('../questions3.json')[difficulty]?.levels || []; // Fetch levels based on difficulty
 
+    const handleBackToDifficulty = () => {
+        navigation.navigate('Difficulty');
+    };
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Select Level</Text>
-            {levels.map((level, index) => (
-                <TouchableOpacity
-                    key={index}
-                    style={styles.button}
-                    onPress={() => navigation.navigate('Game', { questions: level.questions, difficulty: difficulty, level: level.level })}
-                >
-                    <Text style={styles.buttonText}>Level {level.level}</Text>
-                </TouchableOpacity>
-            ))}
-        </View>
+        <ImageBackground
+            source={require('../assets/images/mathgame.png')}
+            style={styles.background}
+        >
+            <TouchableOpacity
+                        style={styles.backButton}
+                        onPress={() => handleBackToDifficulty()}
+                    >
+                        <Text style={styles.backButtonText}>Back</Text>
+            </TouchableOpacity>
+            <View style={styles.container}>
+                {levels.map((level, index) => (
+                    <TouchableOpacity
+                        key={index}
+                        style={styles.button}
+                        onPress={() => navigation.navigate('Game', { questions: level.questions, difficulty: difficulty, level: level.level })}
+                    >
+                        <Text style={styles.buttonText}>Level {level.level}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         gap: 20,
-        backgroundColor: 'blue',
-    },
-    title: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        color: 'white'
     },
     button: {
-        width: 200,
-        padding: 10,
-        backgroundColor: 'transparent',
+        width: 300,
+        padding: 5,
+        backgroundColor: '#002248', // Transparent white background
         borderWidth: 3,
         borderColor: "#FFF",
         borderRadius: 5,
         marginBottom: 20,
-        alignItems: 'center',
+        alignItems: "center",  // Glow color
+        shadowOpacity: 0.5,   // Strength of the glow
+        shadowRadius: 1,     // Spread of the glow
+        elevation: 1,        // For Android shadow
     },
     buttonText: {
-        color: "#FFF",
-        fontSize: 20,
+        color: "#cfd4dd",
+        fontSize: 50,
+        fontFamily: "BebasNeue-Regular",
+    },
+    backButton: {
+        position: 'absolute',
+        top: 40,
+        left: 50,
+        width: 100,
+        padding: 5,
+        backgroundColor: '#002248', // Transparent white background
+        borderWidth: 3,
+        borderColor: "#FFF",
+        borderRadius: 5,
+        marginBottom: 20,
+        alignItems: "center",  // Glow color
+        shadowOpacity: 0.5,   // Strength of the glow
+        shadowRadius: 1,     // Spread of the glow
+        elevation: 1,        // For Android shadow
+    },
+    backButtonText: {
+        color: "#cfd4dd",
+        fontSize: 30,
+        fontFamily: "BebasNeue-Regular",
     }
 });
 
